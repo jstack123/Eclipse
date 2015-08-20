e1b1e832f14e4820b9a4658cc7fa7a20cf7ff18c
jstack123 <Guest@wts10.attlocal.net>
2015-08-19 18:42:45 -0700

    finished grid world and worked on fortune teller

--- workspace/JoeyLevel1/src/GridWorld.java
+++ workspace/JoeyLevel1/src/GridWorld.java
@@ -11,17 +11,33 @@ public class GridWorld {
 		Random random = new Random();
 		World world = new World();
 		world.show();
+		for (int i = 0; i < 1000; i++) {
+			Location location3 = new Location(random.nextInt(10),random.nextInt(10));
+			Flower flowerall = new Flower();
+			if (location3.getRow() %2==0) {
+				flowerall.setColor(Color.RED);
+			} else {
+				flowerall.setColor(Color.WHITE);
+			}
+			world.add(location3, flowerall);
+		}
 		Bug bug = new Bug();
 		Bug bug2 = new Bug(Color.BLUE);
 
 		Location location = new Location(4, 6);
 		Location location2 = new Location(random.nextInt(10),
 				random.nextInt(10));
+	
+		Location left = new Location(location2.getRow(), location2.getCol()-1);
+		Location right = new Location(location2.getRow(), location2.getCol()+1);
 		world.add(location, bug);
 		world.add(location2, bug2);
 		bug2.turn();
 		bug2.turn();
 		Flower flower = new Flower();
-
+		Flower flower2 = new Flower();
+		world.add(left, flower);
+		world.add(right, flower2);
+		
 	}
 }
