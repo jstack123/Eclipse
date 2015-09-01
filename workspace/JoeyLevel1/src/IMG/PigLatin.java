d24c4f954a12d0dce50f215518f0954ddb6f328c
jstack123 <Guest@wts11.attlocal.net>
2015-08-31 18:31:47 -0700

    PIG LATINNNNNNN

---
+++ workspace/JoeyLevel1/src/IMG/PigLatin.java
@@ -0,0 +1,26 @@
+package IMG;
+
+import java.awt.TextField;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class PigLatin {
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		frame.setVisible(true);
+		frame.setName("Joey's Pig Latin Translator");
+		JPanel panel = new JPanel();
+		frame.add(panel);
+		TextField text = new TextField(20);
+		panel.add(text);
+		JButton button = new JButton("translate");
+		panel.add(button);
+		TextField text2 = new TextField(20);
+		panel.add(text2);
+		frame.pack();
+		
+		
+	}
+}
