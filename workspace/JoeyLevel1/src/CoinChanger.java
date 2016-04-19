5da26055ae673daf6cb7c4c7db759b3206e0a545
Nicholas Contreras <nicholasc2000@yahoo.com>
2016-04-18 18:22:39 -0700

    Coin Changer

---
+++ workspace/JoeyLevel1/src/CoinChanger.java
@@ -0,0 +1,41 @@
+
+public class CoinChanger {
+	int numberOfCoins;
+	int coinDenomination;
+
+	public CoinChanger(int numberOfCoins, int coinDenomination) {
+		this.numberOfCoins = numberOfCoins;
+		this.coinDenomination = coinDenomination;
+	}
+
+	int dispenseCoin() {
+		if (numberOfCoins < 0) {
+			System.out.println("I don't have enough coins to dispense another coin.");
+		} else {
+			numberOfCoins--;
+			System.out.println("I am now dispensing a " + coinDenomination);
+		}
+		return coinDenomination;
+	}
+
+	void addCoins(int numberOfCoins) {
+		this.numberOfCoins += numberOfCoins;
+	}
+
+	void addDollar(int numberOfDollars) {
+		this.numberOfCoins += 100 / coinDenomination * numberOfDollars;
+
+	}
+
+	void printCoins() {
+		System.out.println("You have a number of " + numberOfCoins + " coins.");
+	}
+
+	public static void main(String[] args) {
+		CoinChanger coinChanger = new CoinChanger(1, 5);
+		coinChanger.dispenseCoin();
+		coinChanger.addCoins(10);
+		coinChanger.addDollar(2);
+		coinChanger.printCoins();
+	}
+}
